f88f067a2c8666ce4f8cde67f55478ac55cac471
dewandaca <121083307+dewandaca@users.noreply.github.com>
2025-11-25 16:47:25 +0700

    first commit

---
+++ pert5_50422408/src/main/java/com/mycompany/pert5_50422408/Pert5_50422408.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert5_50422408;
+
+/**
+ *
+ * @author Acer
+ */
+public class Pert5_50422408 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ pert5_50422408/src/main/java/com/mycompany/pert5_50422408/repository/MahasiswaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert5_50422408.repository;
+
+import com.mycompany.pert5_50422408.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Acer
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long> {
+
+}
